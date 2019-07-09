dc17538564b1ebdaf2f44b4fd1c283489a3c4ee2
Arthur Conner <arthurconner@me.com>
2019-07-09 13:06:39 -0700

    Now does image similarity

--- Faceplant/ContentView.swift
+++ Faceplant/ContentView.swift
@@ -11,40 +11,80 @@ import SwiftUI
 
 struct FileView : View {
     
-   @ObjectBinding var info:ACFileStatus
-   
+    @ObjectBinding var info:ACFileStatus
+    
     var body: some View {
-        info.loadImage()
         
-       
-        return VStack{
-            if (info.image != nil){
-            Image(info.image!, scale: 60, label: Text("ouch"))
-            } else {
-                 Text("Loading").frame( height: 160)
-            }
-         
+        Image(nsImage: info.image!)
+            .frame(width: ACFileStatus.thumbSize.width * 2, height: ACFileStatus.thumbSize.height * 2).background(Color.orange)
+        
+        
+        
+    }
+    
+}
+
+struct GroupView : View {
+    
+    var group:ACFileGroup
+    
+    var body: some View {
+        
+        //let im = group.members.filter({$0.image != nil}).first?.image
+        
+        return ScrollView(.horizontal, showsIndicators: true){
+            
+            HStack{
+                
+                ForEach(group.members.filter({$0.image != nil})){ x in
+                    Image(nsImage: x.image!).frame(width: ACFileStatus.thumbSize.width , height: ACFileStatus.thumbSize.height ).padding()
+                        .background(Color.white.cornerRadius(8))
+                }
+            }.padding()
+            
+            
             
         }
+        
+        
+        /*
+         
+         return ScrollView(.horizontal, showsIndicators: true){
+         HStack{
+         List(group.members.filter({$0.image != nil})) { landmark in
+         FileView(info: landmark)
+         
+         }
+         }.frame(height: ACFileStatus.thumbSize.height)
+         }.background(Color.yellow)
+         */
+        
     }
     
 }
 
 struct ContentView : View {
     
-    static let foo =  FileLoader("/Volumes/Zoetrope/images/2018/09/Keeper", kinds: [".JPG"], isImage: true)
+    @ObjectBinding var myGroups =  FileLoader("/Volumes/Zoetrope/Keeper", kinds: [".JPG"], isImage: true, post:true)
     
     var body: some View {
-       
-        Group{
-        List(ContentView.foo.allFiles) { landmark in
-            FileView(info: landmark)
-            
-        }
         
-        Text("Hello World")
-           
-        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        Group{
+            if (!myGroups.groups.isEmpty){
+                Slider(value: $myGroups.theshold, from: 1, through: 40, by: 0.5)
+            }
+            
+            ScrollView(.vertical, showsIndicators: true){
+                // VStack{
+                ForEach(myGroups.groups) { landmark in
+                    GroupView(group: landmark)
+                    
+                }.padding()
+                //}.background(Color.purple)
+            }.padding()
+            //  .background(Color.pink)
+            
+        }.padding().frame(maxWidth: .infinity, maxHeight: .infinity)
     }
     
 }
--- Faceplant/FIleSystem.swift
+++ Faceplant/FIleSystem.swift
@@ -10,59 +10,65 @@ import Foundation
 import SwiftUI
 import ImageIO
 import Combine
+import Vision
 
 
 struct FileInfo {
     let path: String
     let date: DateComponents
-
-   
+    
+    
     
     let id: UUID = UUID()
     
     var key:String{
         let y = date.year ?? 2000
         let m = date.month ?? 0
-        return NSString.path(withComponents: ["\(y),\(m)"])
+        return NSString.path(withComponents: ["\(y)","\(m)",(path as NSString).lastPathComponent])
     }
     
-   
+    
+    static func dateURL(_ mUrl:URL,isImage:Bool)->DateComponents?{
+        
+        let fm = FileManager.default
+        
+        guard let det =  try? fm.attributesOfItem(atPath: mUrl.path), let creationDate  = det[FileAttributeKey.creationDate] as? Date else {
+            return nil
+        }
+        
+        if !isImage {
+            let calendar = Calendar.current
+            return  calendar.dateComponents([.year, .month], from: creationDate)
+        }
+        
+        guard let d = CGImageSourceCreateWithURL(mUrl as CFURL , nil),
+            let p = CGImageSourceCopyPropertiesAtIndex(d,0,nil),
+            let ex = (p as NSDictionary)["{Exif}"] as? NSDictionary,
+            let org =  ex["DateTimeOriginal"] as? NSString
+            else   {
+                let calendar = Calendar.current
+                return  calendar.dateComponents([.year, .month], from: creationDate)
+        }
+        
+        let comp = org.components(separatedBy: ":")
+        let year =  Int(comp[0])
+        let month =  Int(comp[1])
+        
+        return  DateComponents(year:year,month: month)
+    }
+    
+    
     
     static func make(path:String,isImage:Bool)->(String,FileInfo)?{
-        let f = FileManager.default
-        var ret:FileInfo?
         
-        do {
-            let attrs = try f.attributesOfItem(atPath: path)
-            
-            if let creationDate = attrs[.creationDate] as? Date{
-                let calendar = Calendar.current
-                ret = FileInfo(path: path,date: calendar.dateComponents([.year, .month], from: creationDate))
-            }
-     
-        } catch {
-            
-        }
+        guard
+            let comp = FileInfo.dateURL(URL(fileURLWithPath: path),isImage: isImage) else { return nil}
         
-        if false,
-            isImage,
-            let url = NSURL(string: path),
-            let imageSource = CGImageSourceCreateWithURL(url, nil),
-            let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?{
- 
-            let exifDict = imageProperties[kCGImagePropertyExifDictionary]
-            print(exifDict as Any)
-         
-            
-        }
+        let r = FileInfo(path: path, date: comp)
+        let fname = (path as NSString).lastPathComponent
         
+        return ("\(r.key)-->\(fname)",r)
         
-        if let r = ret {
-            let fname = (path as NSString).lastPathComponent
-            
-            return ("\(r.key)-->\(fname)",r)
-        }
-        return nil
     }
 }
 
@@ -97,7 +103,7 @@ class ACFileStatus   {
         }
     }
     
-    var image:CGImage?{
+    var image:NSImage?{
         didSet {
             updateMe()
         }
@@ -113,24 +119,56 @@ class ACFileStatus   {
     func dest(root:String)->String{
         
         let last = isKeeper  ? "Keeper" : "Extra"
-        let r = NSString.path(withComponents: [root,info.key,last,(info.path as NSString).lastPathComponent ])
+        
+        let y = info.date.year ?? 2000
+        let m = info.date.month ?? 0
+        
+        let r = NSString.path(withComponents: [root,"\(y)","\(m)",last,(info.path as NSString).lastPathComponent ])
         return r
         
     }
     
+    static let thumbSize = NSSize(width: 128, height: 128)
     
-    func loadImage(){
-        guard (self.image == nil ) else { return }
-        ACFileStatus.queue.async {
-            var sampRect = NSRect(x: 0, y: 0, width: 400, height: 400)
-            
-            if let im = NSImage(contentsOfFile: self.info.path)?.cgImage(forProposedRect: &sampRect, context: NSGraphicsContext.current, hints: nil){
-                self.image = im
-                
-                
+    
+    func makeThumb(){
+        guard self.image == nil else {return}
+        let url = URL(fileURLWithPath: info.path)
+        DispatchQueue.main.async {
+            print("making thumbnail: \(self.info.path)")
+            if let im = NSImage(contentsOf: url){
+                let small = NSImage(size: ACFileStatus.thumbSize)
+                let fromRect = NSRect(x: 0, y: 0, width:im.size.width, height: im.size.height)
+                small.lockFocus()
+                im.draw(in: NSRect(x: 0, y: 0, width: ACFileStatus.thumbSize.width, height: ACFileStatus.thumbSize.height), from: fromRect, operation: .copy, fraction: 1)
+                small.unlockFocus()
+                self.image = small
+                print("stopped thumbnail: \(self.info.path)")
+            } else {
+                print("No image for \(self.info.path)")
             }
         }
     }
+    
+    lazy var features:VNFeaturePrintObservation? = {
+        let url = URL(fileURLWithPath: info.path)
+        print("starting: \(info.path)")
+        
+        
+        
+        let requestHandler = VNImageRequestHandler(url: url, options: [:])
+        let request = VNGenerateImageFeaturePrintRequest()
+        do {
+            
+            try requestHandler.perform([request])
+            print("finishing: \(info.path)")
+            return request.results?.first as? VNFeaturePrintObservation
+        } catch {
+            print("Vision error: \(error)")
+            return nil
+        }
+        
+    }()
     
     
 }
@@ -144,25 +182,59 @@ extension ACFileStatus : BindableObject {
     
 }
 
-
-class FileLoader {
+class ACFileGroup {
+    let members:[ACFileStatus]
     
-    var files: [String: ACFileStatus] = [:]
+    init(_ m: [ACFileStatus]) {
+        members = m
+    }
+}
+
+extension ACFileGroup : Identifiable{
+    public var id:UUID {
+        return members.first?.id ?? UUID()
+    }
+}
+
+class FileLoader  : BindableObject {
+    
+    var groups: [ACFileGroup] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.didChange.send(self)
+            }
+            
+        }
+    }
+    
+    var theshold:Float = 10.0  {
+        didSet {
+            DispatchQueue.main.async {
+                self.didChange.send(self)
+                self.process()
+            }
+            
+        }
+    }
+    
+    private var files:[ACFileStatus] = []
+    
     var source:String
     
+    let didChange = PassthroughSubject<FileLoader, Never>()
     
-    static func contentsOf(_ path:String, kinds:[String], isImage:Bool = true)->[String: ACFileStatus]{
+    static func contentsOf(_ path:String, kinds:[String], isImage:Bool = true)->[ACFileStatus]{
         
         let f = FileManager.default
         let list = kinds.map({$0.uppercased()}).map{ $0.replacingOccurrences(of: ".", with: "")}
         
-      let keep = Set<String>(list)
-        var files: [String: ACFileStatus] = [:]
+        let keep = Set<String>(list)
+        var files: [ACFileStatus] = []
         do {
             for  x in try f.contentsOfDirectory(atPath: path){
                 if keep.contains((x as NSString).pathExtension),
                     let info = ACFileStatus(path: (path as NSString).appendingPathComponent(x), isImage: isImage){
-                    files[info.key] = info
+                    files.append(info)
                 }
             }
         } catch {
@@ -172,10 +244,10 @@ class FileLoader {
         return files
     }
     
-    static func recursive(dirs:[String], kinds:[String], isImage:Bool = true)->[String: ACFileStatus]{
+    static func recursive(dirs:[String], kinds:[String], isImage:Bool = true)->[ ACFileStatus]{
         
         let f = FileManager.default
-        var files: [String: ACFileStatus] = [:]
+        var files: [ACFileStatus] = []
         
         guard !dirs.isEmpty else {
             return files
@@ -190,8 +262,8 @@ class FileLoader {
                     
                     if (f.fileExists(atPath: full, isDirectory: &isDir) ){
                         let adds = FileLoader.recursive(dirs: [dir], kinds: kinds, isImage: isImage)
-                        for (k,v) in adds {
-                            files[k] = v
+                        for v in adds {
+                            files.append(v)
                         }
                     }
                     
@@ -202,8 +274,8 @@ class FileLoader {
             
             let adds = FileLoader.contentsOf(dir, kinds: kinds, isImage: isImage)
             
-            for (k,v) in adds {
-                files[k] = v
+            for v in adds {
+                files.append(v)
             }
             
         }
@@ -212,19 +284,84 @@ class FileLoader {
         
     }
     
-    init(_ path:String, kinds:[String], isImage:Bool = true){
+    func process(){
+        
+        DispatchQueue.global(qos: .background).async {
+            guard !self.files.isEmpty else { return }
+            
+            let f =  self.files.sorted(by: {$0.info.path < $1.info.path})
+            
+            for x in f {
+                x.makeThumb()
+            }
+            
+            var ret:[ACFileGroup] = []
+            let unknown = f.filter{ $0.features == nil }
+            if !unknown.isEmpty {
+                ret.append(ACFileGroup(unknown))
+            }
+            
+            let known = f.filter{ $0.features != nil }
+            var i = 0
+            var current:[ACFileStatus] = []
+            while (i < known.count) {
+                let check = known[i]
+                i += 1
+                if let goodExample = current.last{
+                    do {
+                        var distance = Float(0)
+                        try goodExample.features!.computeDistance(&distance, to: check.features!)
+                        
+                        
+                        print("distance from \(goodExample.info.key) to \(check.info.key) is \(distance)")
+                        if distance < self.theshold {
+                            current.append(check)
+                        } else {
+                            ret.append(ACFileGroup(current))
+                            current = [check]
+                        }
+                        
+                    } catch {
+                        
+                        ret.append(ACFileGroup(current))
+                        current = [check]
+                        
+                    }
+                    
+                } else {
+                    current.append(check)
+                }
+                
+            }
+            
+            ret.append(ACFileGroup(current))
+            print("we have \(ret.count) groups for \(self.theshold)")
+            DispatchQueue.main.async {
+                self.groups = ret
+            }
+            
+            
+        }
+        
+    }
+    
+    init(_ path:String, kinds:[String], isImage:Bool = true,post:Bool=false){
         source = path
         files = FileLoader.contentsOf(path, kinds:kinds, isImage:isImage)
+        if (post){
+            process()
+        }
     }
     
-    init(recursive path:String, kinds:[String], isImage:Bool = true){
+    init(recursive path:String, kinds:[String], isImage:Bool = true,post:Bool=false){
         source = path
         files = FileLoader.recursive(dirs:[source], kinds:kinds, isImage:isImage)
+        if (post){
+            process()
+        }
     }
     
-   var allFiles:[ACFileStatus]{
-        return files.values.sorted(by: {$0.info.path < $1.info.path})
-    }
+    
     
     
 }
